1a95ff3bfee373ede1d342fa9fc249caa8e0cf1b
Raimon Esteve <raimonesteve@gmail.com>
2022-03-22 18:43:30 +0100

    Check rule only if _check_access is set and enforce companies rule issue4080

--- tests/test_employee_leave.py
+++ tests/test_employee_leave.py
@@ -5,9 +5,10 @@ import doctest
 import trytond.tests.test_tryton
 from trytond.tests.test_tryton import ModuleTestCase
 from trytond.tests.test_tryton import doctest_teardown, doctest_checker
+from trytond.modules.company.tests import CompanyTestMixin
 
 
-class TestCase(ModuleTestCase):
+class TestCase(CompanyTestMixin, ModuleTestCase):
     'Test module'
     module = 'employee_leave'
 
