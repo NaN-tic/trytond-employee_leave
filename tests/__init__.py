c3e331b876f8edaafef4263a0df358b4225d0acc
Jared Esparza <jared.esparza@nan-tic.com>
2022-04-26 13:20:03 +0200

    Replace test setuptools command by unittest discover

--- tests/__init__.py
+++
@@ -1,8 +0,0 @@
-# The COPYRIGHT file at the top level of this repository contains the full
-# copyright notices and license terms.
-try:
-    from trytond.modules.employee_leave.tests.test_employee_leave import suite
-except ImportError:
-    from .test_employee_leave import suite
-
-__all__ = ['suite']
